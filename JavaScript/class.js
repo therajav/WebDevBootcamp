17446b0780e1ff1db914bbfc0b0728db72ded8af
therajav <mobiraja2003@gmail.com>
2024-05-15 06:27:50 +0530

    Class Extends and Super Keywords

---
+++ JavaScript/class.js
@@ -0,0 +1,21 @@
+class Pet{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    eat() {
+        return `${this.name} is eating!`
+    }
+
+}
+
+class Cat extends Pet {
+    meow() {
+        return 'Meow!';
+    }
+}
+class Dog extends Pet{
+    bark(){
+        return 'Woof!';
+    }
+}
