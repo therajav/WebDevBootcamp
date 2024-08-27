af0fb54db408210ebc21144e53a776bf7aff3bf6
therajav <mobiraja2003@gmail.com>
2024-08-27 06:44:25 +0530

    RESTful CRUD

--- REST/index.js
+++ REST/index.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(methodOverride('_method'));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
-const comments = [
+let comments = [
     {
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is funny'
     },
     {
+        id: uuid(),
         username: 'Skyler',
         comment: 'That is good'
     },
     {
+        id: uuid(),
         username: 'Robby',
         comment: 'Hahaha'
     },
     {
+        id: uuid(),
         username: 'Tom',
         comment: 'Hello all'
     },
@@ -36,10 +43,35 @@ app.get('/comments/new',(req,res) => {
 
 app.post('/comments',(req,res) => {
     const {username, comment} = req.body;
-    comments.push({username,comment});
-    console.log(req.body);
-    res.send("It worked!");
+    comments.push({username,comment, id: uuid()});
+    res.redirect('/comments')
 });
+
+app.get('/comments/:id',(req,res) => {
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id );
+    res.render('comments/show',{comment})
+});
+
+app.get('/comments/:id/edit',(req,res) => {
+    const {id} = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit',{comment});
+});
+
+app.patch('/comments/:id', (req,res) => {
+    const {id} = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments');
+})
+
+app.delete('/comments/:id', (req,res) => {
+    const {id} = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
 
 app.get('/tacos',(req,res) => {
     res.send('GET /tacos respose');
